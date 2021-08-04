89c5952115a72ba018380cc1456ce1af62497075
Dzmirty Apanasevich <dmitry.apan@yandex.by>
2021-08-04 11:36:33 +0300

    change ws

--- src/Data.js
+++ src/Data.js
@@ -3,7 +3,7 @@
 const base = 'https://taskmanagerserverapp.herokuapp.com';
 
 //export let urlWebSocket = 'ws://localhost:8080';
-export let urlWebSocket = 'ws://taskmanagerserverapp.herokuapp.com';
+export let urlWebSocket = 'taskmanagerserverapp.herokuapp.com';
 export let urlCardData = `${base}/cards`;
 export let urlCardCreate = `${base}/card`;
 export let urlCardSettings = `${base}/settings`;
